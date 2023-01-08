545a171024f4cbdf32b677b606c2294f7b7b341a
KodiRealDebridIsrael <KodiRealDebridIsrael@github.com>
2023-01-09 01:32:14 +0200

    Add guide addon!

--- build_guides_addon/plugin.program.kodirealdebridisraelguides/remote_script_main.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-# Download the script from the URL
-response = urllib.request.urlopen('http://example.com/script.py')
-script = response.read()
-
-# Execute the script
-exec(script)
